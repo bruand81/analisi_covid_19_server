e1a8f2c92c2454888f9c0dbdf3852e48e353a154
bruand <andrea.bruno@antaresnet.org>
2020-11-03 11:30:43 +0100

    Bugfixex

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'regioni', views.RegioniItalianeViewSet, 'regioni')
-router.register(r'province', views.ProvinceItalianeViewSet, 'province')
-router.register(r'riepilogo', views.RiepilogoRegioniViewSet, 'riepilogo')
-router.register(r'list', views.RegioniListViewsSet, 'list')
+router.register(r'regioni', views.RegioniItalianeViewSet)
+router.register(r'province', views.ProvinceItalianeViewSet)
+router.register(r'riepilogo', views.RiepilogoRegioniViewSet)
+router.register(r'list', views.RegioniListViewsSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
