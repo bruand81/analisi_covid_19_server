ba5e1b21a646da29cd4f97ea64b34c2f5c51b05f
bruand <andrea.bruno@antaresnet.org>
2021-02-04 08:22:02 +0100

    Aggiunta TODO List e miglioramento errori in logging

--- api/management/commands/updatedata.py
+++ api/management/commands/updatedata.py
@@ -4,21 +4,27 @@ from models.methods import *
 import logging
 import time
 from django.conf import settings
+import sys
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logging.getLogger().info('Invoked update')
-        lock_file = f'analisi_covid_update_{datetime.today().strftime("%Y%m%d")}.lock'
-        if not os.path.exists(lock_file):
-            logging.getLogger().info('Starting update')
-            updated = False
-            while not updated:
-                updated = update_db()
-                if updated:
-                    logging.getLogger().info(f'Database updated')
-                else:
-                    logging.getLogger().info(f'Database not updated. I will try again in {settings.UPDATE_INTERVAL} seconds')
-                    time.sleep(settings.UPDATE_INTERVAL)
-            logging.getLogger().info('Update completed')
-            if os.path.exists(lock_file):
-                os.remove(lock_file)
+        lockfile = f'analisi_covid_update_{datetime.today().strftime("%Y%m%d")}.lock'
+        try:
+            if not os.path.exists(lockfile):
+                logging.getLogger().info('Starting update')
+                updated = False
+                while not updated:
+                    updated = update_db()
+                    if updated:
+                        logging.getLogger().info(f'Database updated')
+                    else:
+                        logging.getLogger().info(
+                            f'Database not updated. I will try again in {settings.UPDATE_INTERVAL} seconds')
+                        time.sleep(settings.UPDATE_INTERVAL)
+                logging.getLogger().info('Update completed')
+                if os.path.exists(lockfile):
+                    os.remove(lockfile)
+        except:
+            logging.getLogger().exception(f"Error importing data", exc_info=sys.exc_info())
